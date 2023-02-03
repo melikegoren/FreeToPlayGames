e3eda3fc090601ac83ff59f3f97b2c7fd7223a12
Melike Goren <melike.goren@outlook.com>
2023-02-03 19:01:05 +0300

    Splash Screen added

--- app/src/main/java/com/example/freetoplaygames/presentation/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/example/freetoplaygames/presentation/splashscreen/SplashScreen.kt
@@ -1,10 +1,13 @@
 package com.example.freetoplaygames.presentation.splashscreen
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.freetoplaygames.R
 
 
@@ -20,6 +23,10 @@ class SplashScreen : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            findNavController().navigate(R.id.action_splashScreen_to_gamesFragment)
+        }, 2000)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash_screen, container, false)
     }
